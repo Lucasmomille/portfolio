1c07da7174f6097c401ce7c29c0368df00ab96d0
Lucas <lucas.moquet95@gmail.com>
2021-08-03 18:08:26 +0200

    move dark mode other branch

--- components/Layout.js
+++ components/Layout.js
@@ -6,21 +6,6 @@ import useDarkMode from "../hooks/useDarkMode";
 
 export default function Layout({ children, page }) {
 
-    const { theme, setTheme } = useState('dark');
-    //const { count, setCount } = useState(0)
-    const colorTheme = theme === "dark" ? "light" : "dark";
-
-    const toggleTheme = () => {
-        setTheme('light')
-    }
-
-    useEffect(() => {
-        const root = window.document.documentElement;
-        root.classList.remove(colorTheme);
-        root.classList.add(theme);
-    }, [theme, colorTheme]);
-
-    // const [colorTheme, setTheme] = useDarkMode();
 
     return (
         <div className="bg-white font-nunito min-h-screen dark:bg-black text-dark dark:text-white">
@@ -45,18 +30,7 @@ export default function Layout({ children, page }) {
                     <Link href="/cv">
                         <a href="" className="">CV</a>
                     </Link>
-                    <p className="cursor-pointer" onClick={toggleTheme}>
-                        {colorTheme === "light" ? (
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-                            </svg>
-                        ) : (
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-                            </svg>
-                        )
-                        }
-                    </p>
+
 
                 </nav>
             </header>
