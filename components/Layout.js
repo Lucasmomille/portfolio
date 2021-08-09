8de5825682eb06e3282c5c5140b46ef0247cf59a
Lucas <lucas.moquet95@gmail.com>
2021-08-09 16:25:40 +0200

    darkMode almost

--- components/Layout.js
+++ components/Layout.js
@@ -4,6 +4,26 @@ import Image from "next/image";
 import React, { useEffect, useState } from 'react';
 
 export default function Layout({ children, page }) {
+    const [theme, setTheme] = useState("dark");
+
+    const colorTheme = theme === "dark" ? "light" : "dark";
+
+    const toggleTheme = () => {
+        if (theme === "dark") {
+            setTheme('light')
+        } else {
+            setTheme('dark')
+        }
+
+    }
+
+    useEffect(() => {
+        const root = window.document.documentElement;
+        root.classList.remove(colorTheme);
+        root.classList.add(theme);
+    }, [theme, colorTheme]);
+
+
     return (
         <div className="bg-white font-nunito pb-12 dark:bg-black text-dark dark:text-white">
             <Head>
@@ -25,6 +45,20 @@ export default function Layout({ children, page }) {
                         <a href="" className="">CV</a>
                     </Link>
                 </nav>
+
+                <p className="cursor-pointer" onClick={toggleTheme}>
+                    {colorTheme === "light" ? (
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                        </svg>
+                    ) : (
+
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                        </svg>
+                    )
+                    }
+                </p>
             </header>
             {children}
             <footer className="bg-primary w-full py-3 px-8 text-white absolute bottom-0">
