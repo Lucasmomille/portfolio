f713e73b5e8cd35d6d37b9ea2f87778695d05134
Lucas <lucas.moquet95@gmail.com>
2021-08-09 16:27:48 +0200

    darkMode almost done

--- components/Layout.js
+++ components/Layout.js
@@ -40,25 +40,26 @@ export default function Layout({ children, page }) {
                     </a>
                 </Link>
 
-                <nav className="flex flex-wrap w-1/12 ">
+                <nav className="flex flex-wrap w-2/12 ">
                     <Link href="/cv">
                         <a href="" className="">CV</a>
                     </Link>
+                    <p className="cursor-pointer" onClick={toggleTheme}>
+                        {colorTheme === "light" ? (
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                            </svg>
+                        ) : (
+
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                            </svg>
+                        )
+                        }
+                    </p>
                 </nav>
 
-                <p className="cursor-pointer" onClick={toggleTheme}>
-                    {colorTheme === "light" ? (
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-                        </svg>
-                    ) : (
 
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-                        </svg>
-                    )
-                    }
-                </p>
             </header>
             {children}
             <footer className="bg-primary w-full py-3 px-8 text-white absolute bottom-0">
